3b923a0f999299b607fe5531faccc20a42b3ab08
kaylor <kaylor.chen@qq.com>
2023-09-12 22:43:48 +0800

    Update

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys DE86F73ED9E67D5E  
-echo "deb [arch=amd64] http://debian.kaylordut.com:50000/focal-backports focal-backports main" | sudo tee /etc/apt/sources.list.d/debian-kaylordut.list > /dev/null
+echo "deb [arch=amd64] http://42.194.233.81:60000/focal-backports focal-backports main" | sudo tee /etc/apt/sources.list.d/debian-kaylordut.list > /dev/null
 mv jammy.list /etc/apt/sources.list.d/
 sudo apt update
 sudo apt install -y python3-apt
@@ -15,13 +15,13 @@ do
     echo -e "\033[35m Building $pkg_name \033[0m"
     directory_name=$(apt showsrc $pkg_name | grep .orig.tar. | grep -m 1 "" | awk '{print $3}' | awk -F '.orig.' '{print $1}' | tr '_' '-')
     echo directory is $directory_name
-    # apt source $pkg_name
-    # pwd
-    # cd $directory_name
-    # DEB_BUILD_OPTIONS='--parallel' fakeroot debian/rules binary
-    # cd ..
-    # mv *deb deb_packages/
-    # sudo rm ${directory_name}* -rf
-    # sudo apt install -y ./deb-packages/*.deb
+    apt source $pkg_name
+    ls 
+    cd $directory_name
+    DEB_BUILD_OPTIONS='--parallel' fakeroot debian/rules binary
+    cd ..
+    mv *deb deb_packages/
+    sudo rm ${directory_name}* -rf
+    sudo apt install -y ./deb-packages/*.deb
   fi
 done
